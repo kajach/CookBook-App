20cacc74340d3b0742119f3d2d75225e6663992f
Kaja Chlostowska <chlostowska.kaja@gmail.com>
2018-05-07 03:18:37 +0100

    Add RecipeDetail page and view model. Add banner image.

---
+++ CookBook/CookBook/CookBook/Views/RecipeDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CookBook.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookBook.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RecipeDetailPage : ContentPage
+	{
+		public RecipeDetailPage ()
+		{
+			InitializeComponent ();
+
+            BindingContext = new RecipeDetailViewModel();
+        }
+	}
+}
